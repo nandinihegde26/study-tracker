e3d82f70157c7533ab906f1b83e5541d90e9906f
Nandini Hegde <137638389+nandinihegde26@users.noreply.github.com>
2025-07-19 01:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from Render!"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    return render_template("index.html")
 
 
 
